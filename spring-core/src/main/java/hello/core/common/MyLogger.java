d69d9ad06cd3b66985e76bb7bc6b290febb59a49
jack-yu-dw <dongwook1269@gmail.com>
2022-07-28 21:13:39 +0900

    0728 : 스프링 기본 - 빈 스코

---
+++ spring-core/src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,37 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyLogger { // log 를 출력하기 위한 MyLogger 클래스
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + "[" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        // global 하게 unique 한 id 가 생성된다. (전세계적으로)
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create:" + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close:" + this);
+    }
+}
