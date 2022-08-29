c8341d94b36bccbf6f7912e782b25110254ac29d
jack-yu-dw <dongwook1269@gmail.com>
2022-08-29 19:46:53 +0900

    0829 : spring mvc1 - HttpServletRequest

--- spring-mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
+++ spring-mvc1/servlet/src/main/java/hello/servlet/basic/request/RequestHeaderServlet.java
@@ -7,9 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.Enumeration;
 
-@WebServlet(name = "requestHeaderServlet", urlPatterns = "/request-header")
+@WebServlet(name = "requestHeaderServelet", urlPatterns = "/request-header")
 public class RequestHeaderServlet extends HttpServlet {
 
     @Override
@@ -20,19 +19,22 @@ public class RequestHeaderServlet extends HttpServlet {
         printEtc(request);
     }
 
+    //start line 정보
     private void printStartLine(HttpServletRequest request) {
         System.out.println("--- REQUEST-LINE - start ---");
 
         System.out.println("request.getMethod() = " + request.getMethod()); //GET
-        System.out.println("request.getProtocal() = " + request.getProtocol()); //HTTP/1.1
+        System.out.println("request.getProtocol() = " + request.getProtocol()); //HTTP/1.1
         System.out.println("request.getScheme() = " + request.getScheme()); //http
         // http://localhost:8080/request-header
         System.out.println("request.getRequestURL() = " + request.getRequestURL());
-        // /request-test
+        // /request-header
         System.out.println("request.getRequestURI() = " + request.getRequestURI());
         //username=hi
-        System.out.println("request.getQueryString() = " + request.getQueryString());
+        System.out.println("request.getQueryString() = " +
+                request.getQueryString());
         System.out.println("request.isSecure() = " + request.isSecure()); //https 사용 유무
+
         System.out.println("--- REQUEST-LINE - end ---");
         System.out.println();
     }
@@ -41,21 +43,22 @@ public class RequestHeaderServlet extends HttpServlet {
     private void printHeaders(HttpServletRequest request) {
         System.out.println("--- Headers - start ---");
 
-/*
-        Enumeration<String> headerNames = request.getHeaderNames();
-        while (headerNames.hasMoreElements()) {
-            String headerName = headerNames.nextElement();
-            System.out.println(headerName + ": " + headerName);
-        }
-*/
-
+    /* 예전 방식
+      Enumeration<String> headerNames = request.getHeaderNames();
+      while (headerNames.hasMoreElements()) {
+          String headerName = headerNames.nextElement();
+          System.out.println(headerName + ": " + request.getHeader(headerName));
+      }
+    */
         request.getHeaderNames().asIterator()
-                .forEachRemaining(headerName -> System.out.println(headerName + ": " + headerName));
+                .forEachRemaining(headerName -> System.out.println(headerName + ":"
+                        + request.getHeader(headerName)));
 
         System.out.println("--- Headers - end ---");
         System.out.println();
     }
 
+    //Header 편리한 조회
     private void printHeaderUtils(HttpServletRequest request) {
         System.out.println("--- Header 편의 조회 start ---");
         System.out.println("[Host 편의 조회]");
@@ -87,19 +90,17 @@ public class RequestHeaderServlet extends HttpServlet {
     }
 
     //기타 정보
-    private void printEtc(HttpServletRequest request) {
-        System.out.println("--- 기타 조회 start ---");
-
+    private void printEtc(HttpServletRequest request) { System.out.println("--- 기타 조회 start ---");
         System.out.println("[Remote 정보]");
-        System.out.println("request.getRemoteHost() = " + request.getRemoteHost()); //
-        System.out.println("request.getRemoteAddr() = " + request.getRemoteAddr()); //
-        System.out.println("request.getRemotePort() = " + request.getRemotePort()); //
+        System.out.println("request.getRemoteHost() = " + request.getRemoteHost());
+        System.out.println("request.getRemoteAddr() = " + request.getRemoteAddr());
+        System.out.println("request.getRemotePort() = " + request.getRemotePort());
         System.out.println();
 
         System.out.println("[Local 정보]");
-        System.out.println("request.getLocalName() = " + request.getLocalName()); //
-        System.out.println("request.getLocalAddr() = " + request.getLocalAddr()); //
-        System.out.println("request.getLocalPort() = " + request.getLocalPort()); //
+        System.out.println("request.getLocalName() = " + request.getLocalName());
+        System.out.println("request.getLocalAddr() = " + request.getLocalAddr());
+        System.out.println("request.getLocalPort() = " + request.getLocalPort());
 
         System.out.println("--- 기타 조회 end ---");
         System.out.println();
