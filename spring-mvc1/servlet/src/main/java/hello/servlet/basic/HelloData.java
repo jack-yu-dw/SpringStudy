387d24da79b2094b69b4d28b6d80c2dab993e7e2
jack-yu-dw <dongwook1269@gmail.com>
2022-08-30 18:04:29 +0900

    0830 : spring mvc1 - Object for JSON parsing

--- spring-mvc1/servlet/src/main/java/hello/servlet/basic/HelloData.java
+++ spring-mvc1/servlet/src/main/java/hello/servlet/basic/HelloData.java
@@ -5,7 +5,7 @@ import lombok.Setter;
 
 @Getter @Setter
 public class HelloData {
-    // Json 형식으로 파싱할 수 있는 객체
+    // Json 형식으로 Parsing 할 수 있는 객체
 
     private String username;
     private int age;
