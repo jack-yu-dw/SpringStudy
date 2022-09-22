3e6ea7d4f205ac701b2d70b6f62a35fb10416f1c
jack-yu-dw <dongwook1269@gmail.com>
2022-09-22 15:06:44 +0900

    0922 : spring mvc1 - web page ; add @ModelAttribute

--- spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,9 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -39,10 +37,71 @@ public class BasicItemController {
         return "basic/item";
     }
 
-    // addForm 과 save 에서 URL 은 서로 같게, 대신 HTTP method 를 통해 서로 기능 구분해주는 것.
+    // addForm 과 addItem 에서 URL 은 서로 같게, 대신 HTTP method 를 통해 서로 기능 구분해주는 것.
     @GetMapping("/add")
     public String addForm() {
         return "basic/addForm";
+    }
+
+    //@PostMapping("/add")  // 중복 매핑으로 인한 오류 방지
+    public String addItemV1(@RequestParam String itemName,
+                       @RequestParam int price,
+                       @RequestParam Integer quantity,
+                       Model model) {
+        Item item = new Item();
+        item.setItemName(itemName);
+        item.setPrice(price);
+        item.setQuantity(quantity);
+
+        itemRepository.save(item);
+
+        model.addAttribute("item", item);
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute - 요청 파라미터 처리
+     * @ModelAttribute 는 Item 객체를 생성하고, 요청 파라미터의 값을 프로퍼티 접근법(setXxx)으로 입력해준다.
+     *
+     * @ModelAttribute - Model 추가
+     * @ModelAttribute 는 중요한 한가지 기능이 더 있는데, 바로 모델(Model)에 @ModelAttribute 로 지정한 객체를 자동으로 넣어준다.
+     * 지금 코드를 보면 model.addAttribute("item", item) 가 주석처리 되어 있어도 잘 동작하는 것을 확인할 수 있다.
+     * 모델에 데이터를 담을 때는 이름이 필요하다. 이름은 @ModelAttribute 에 지정한 name(value) 속성을 사용한다.
+     * 만약 다음과 같이 @ModelAttribute 의 이름을 다르게 지정하면 다른 이름으로 모델에 포함된다.
+     * @ModelAttribute("hello") -> Item item 이름을 hello 로 지정
+     * model.addAttribute("hello", item); -> 모델에 hello 이름으로 저장
+     */
+    //@PostMapping("/add")
+    public String addItemV2(@ModelAttribute("item") Item item, Model model) {
+
+        itemRepository.save(item);
+//        model.addAttribute("item", item); //자동 추가, 생략 가능
+
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute name 생략 가능
+     * model.addAttribute(item); 자동 추가, 생략 가능
+     * 생략시 model에 저장되는 name은 클래스명 첫글자만 소문자로 등록
+     * Item -> item
+     */
+    //@PostMapping("/add")
+    public String addItemV3(@ModelAttribute Item item) {
+
+        itemRepository.save(item);
+        return "basic/item";
+    }
+
+    /**
+     * @ModelAttribute 자체 생략 가능
+     * model.addAttribute(item) 자동 추가
+     */
+    @PostMapping("/add")
+    public String addItemV4(Item item) {
+        itemRepository.save(item);
+        return "basic/item";
     }
 
     /**
