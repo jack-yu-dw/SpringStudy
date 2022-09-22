e2d3fbf8d9b56d84d31c4d42b439e2eec2cb1ce7
jack-yu-dw <dongwook1269@gmail.com>
2022-09-22 17:04:43 +0900

    0922 : spring mvc1 - web page ; RedirectAttributes

--- spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ spring-mvc1/item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.annotation.PostConstruct;
 import java.util.List;
@@ -114,11 +115,30 @@ public class BasicItemController {
      * 상품 등록 처리 이후에 뷰 템플릿이 아니라 상품 상세 화면으로 리다이렉트 하도록 코드를 작성해보자.
      * 이런 문제 해결 방식을 PRG Post/Redirect/Get 라 한다.
      */
-    @PostMapping("/add")
+    //@PostMapping("/add")
     public String addItemV5(Item item) {
         itemRepository.save(item);
         return "redirect:/basic/items/" + item.getId();
         // +item.getId() 처럼 URL에 변수를 더해서 사용하는 것은 URL 인코딩이 안되기 때문에 위험하므로 RedirectAttributes 를 사용해야 한다.
+    }
+
+    /**
+     * RedirectAttributes
+     * RedirectAttributes 를 사용하면 URL 인코딩도 해주고, pathVarible , 쿼리 파라미터까지 처리해준다.
+     *
+     * redirect:/basic/items/{itemId}
+     *  pathVariable 바인딩: {itemId}
+     *  나머지는 쿼리 파라미터로 처리: ?status=true
+     *
+     * 실행해보면 다음과 같은 리다이렉트 결과가 나온다.
+     * http://localhost:8080/basic/items/3?status=true
+     */
+    @PostMapping("/add")
+    public String addItemV6(Item item, RedirectAttributes redirectAttributes) {
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/basic/items/{itemId}";
     }
 
 
